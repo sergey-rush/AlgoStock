a258df25b908bba211ec1720a366dff954b9bb38
Sergey Rush <sergey.rush@hotmail.com>
2018-12-18 01:59:44 +0300

    String nth replaced

--- Core/Stock.py
+++ Core/Stock.py
@@ -17,6 +17,29 @@ class Stock:
     def __lt__(self, other):
         return self.ticker < other.ticker
 
+    def replace_nth_occurance(input, old_value, new_value, count):
+        """ Replace nth occurance of a string with another string
+        """
+        #input.replace(old_value, new_value, count)
+        for i in range(count):
+            if i == 2:
+                input.replace(old_value, new_value, i)
+        return input
+
+    def nth_repl_all(s, sub, repl, nth):
+        find = s.find(sub)
+        # loop util we find no match
+        i = 1
+        while find != -1:
+            # if i  is equal to nth we found nth matches so replace
+            if i == nth:
+                s = s[:find]+repl+s[find + len(sub):]
+                i = 0
+            # find + len(sub) + 1 means we start after the last match
+            find = s.find(sub, find + len(sub) + 1)
+            i += 1
+        return s
+
 # sort usage:
 # card_sorted = sorted(self.hand_cards) - sorted object list
 # rank_sorted = [c.rank for c in card_sorted] - sorted rank values
--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 import os
 import csv
+import matplotlib.pyplot as plt
 from Core.Stock import Stock
 from Data.DataManager import DataManager
 
@@ -10,7 +11,13 @@ issuer_list = data_manager.get_issuers()
 for issuer in issuer_list:
     print("\n{} {} {}\n".format(issuer.id, issuer.ticker, issuer.name))
     stock_list = data_manager.get_stocks_by_issuer_id(issuer.id)
+    high_stocks = []
     for stock in stock_list:
         print("{} {} {} {} {} {} {} {} {}".format(stock.id, stock.ticker, stock.per, stock.trade_date, stock.open, stock.high, stock.low, stock.close, stock.volume))
+        rpl = Stock.nth_repl_all(stock.high, '.', '', 2)
+        high_stocks.append(float(rpl))
+    plt.plot(high_stocks)
+    plt.show()
 
 print("\nCompleted")
+
